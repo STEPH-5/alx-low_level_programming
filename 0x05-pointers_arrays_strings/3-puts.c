2c2212d5839cf1768d86a0716dce3e55f838e42f
STEPH-5 <stephhoopper@gmail.com>
2023-06-27 18:12:06 -0700

    This is my alx project

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts - Prints a string followed by a new line.
+ * @str: Pointer to a string.
+ */
+void _puts(char *str)
+{
+        for (; *str != '\0'; str++)
+        {
+        _putchar(*str);
+        }
+        _putchar('\n');
+}
